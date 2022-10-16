7eb5f512df0b5ee797a7579734f1eecfd125c794
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-10-16 19:55:26 +0200

    cleanup: Move error.rs to toplevel in gamestreaming_webrtc crate

--- gamestreaming_webrtc/src/error.rs
+++ gamestreaming_webrtc/src/error.rs
@@ -2,6 +2,8 @@ use enumflags2::{BitFlag, FromBitsError};
 
 use thiserror::Error;
 
+use crate::api::GssvApiError;
+
 #[derive(Error, Debug)]
 pub enum PacketError {
     #[error("Unknown error")]
@@ -15,4 +17,12 @@ where
 {
     #[error(transparent)]
     DeserializeError(#[from] FromBitsError<T>),
+}
+
+#[derive(Error, Debug)]
+pub enum GsError {
+    #[error(transparent)]
+    ApiError(#[from] GssvApiError),
+    #[error("Unknown error")]
+    Unknown,
 }
