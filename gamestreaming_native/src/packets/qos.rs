9ab5a6419f964ef23d7e4dc666cfea07916f80a8
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-10-22 21:38:58 +0200

    refactor: Port QoS packets to deku de/serializer

--- gamestreaming_native/src/packets/qos.rs
+++ gamestreaming_native/src/packets/qos.rs
@@ -1,4 +1,7 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+use deku::prelude::*;
+
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
+#[deku(type = "u32")]
 pub enum QosPacketType {
     ServerHandshake = 1,
     ClientHandshake = 2,
@@ -8,13 +11,15 @@ pub enum QosPacketType {
     ClientPolicy = 6,
 }
 
-bitflags! {
-    pub struct QosControlFlags : u8 {
-        const REINITIALIZE = 0x01;
-    }
+#[derive(Debug, Clone, DekuRead, DekuWrite, Eq, PartialEq, Default)]
+pub struct QosControlFlags{
+    // Bit 1 / Mask 0x01
+    #[deku(pad_bits_before="7", bits="1")]
+    pub reinitialize: bool,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct QosServerPolicy {
     pub schema_version: u32,
     pub policy_length: u32,
@@ -23,40 +28,86 @@ pub struct QosServerPolicy {
     pub fragment_size: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct QosServerHandshake {
     pub protocol_version: u32,
-    pub min_supported_client_version: Option<u32>, // if protocol_version >= 1
+    #[deku(cond = "*protocol_version >= 1")]
+    pub min_supported_client_version: Option<u32>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct QosClientPolicy {
     pub schema_version: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct QosClientHandshake {
     pub protocol_version: u32,
     pub initial_frame_id: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct QosControl {
     pub flags: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct QosData {
     pub flags: u32,
     pub frame_id: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum QosPacket {
-    ServerHandshake(QosServerHandshake),
-    ClientHandshake(QosClientHandshake),
-    Control(QosControl),
-    Data(QosData),
-    ServerPolicy(QosServerPolicy),
-    ClientPolicy(QosClientPolicy),
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
+pub struct QosPacket {
+    packet_type: QosPacketType,
+    #[deku(cond = "*packet_type == QosPacketType::ServerHandshake")]
+    server_handshake: Option<QosServerHandshake>,
+    #[deku(cond = "*packet_type == QosPacketType::ClientHandshake")]
+    client_handshake: Option<QosClientHandshake>,
+    #[deku(cond = "*packet_type == QosPacketType::Control")]
+    control: Option<QosControl>,
+    #[deku(cond = "*packet_type == QosPacketType::Data")]
+    data: Option<QosData>,
+    #[deku(cond = "*packet_type == QosPacketType::ServerPolicy")]
+    server_policy: Option<QosServerPolicy>,
+    #[deku(cond = "*packet_type == QosPacketType::ClientPolicy")]
+    client_policy: Option<QosClientPolicy>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_qos_control_flags() {
+        fn create_flag(val: u32) -> QosControlFlags {
+            let val_bytes: [u8; 4] = val.to_le_bytes();
+            let (_, flags) =
+                QosControlFlags::from_bytes((&val_bytes, 0)).expect("Failed to create flags");
+
+            flags
+        }
+        assert!(!create_flag(0x00).reinitialize);
+        assert!(create_flag(0x01).reinitialize);
+    }
+
+    #[test]
+    fn serialize_qos_control_flags() {
+        fn get_value(flags: QosControlFlags) -> u8 {
+            let val = flags
+                .to_bytes()
+                .expect("Failed to convert to bytes");
+            assert_eq!(val.len(), 1);
+            val[0]
+        }
+
+        let none = QosControlFlags::default();
+        let reinitialize = QosControlFlags {
+            reinitialize: true,
+            ..Default::default()
+        };
+
+        assert_eq!(get_value(none), 0x00);
+        assert_eq!(get_value(reinitialize), 0x01);
+    }
 }
