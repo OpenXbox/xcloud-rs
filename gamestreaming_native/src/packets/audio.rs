e8a6877e4897d40c5668225ce5239359b6ede759
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-10-22 16:55:22 +0200

    refactor: Port audio packets to deku de/serializer

--- gamestreaming_native/src/packets/audio.rs
+++ gamestreaming_native/src/packets/audio.rs
@@ -1,4 +1,7 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+use deku::prelude::*;
+
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
+#[deku(type = "u32")]
 pub enum AudioPacketType {
     ServerHandshake = 1,
     ClientHandshake = 2,
@@ -6,64 +9,83 @@ pub enum AudioPacketType {
     Data = 4,
 }
 
-#[repr(u32)]
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
+#[deku(type = "u32")]
 pub enum AudioCodec {
     Opus = 0,
     Pcm = 1,
     Aac = 2,
 }
 
-bitflags! {
-    pub struct AudioControlFlags : u32 {
-        const STOP_STREAM = 0x08;
-        const START_STREAM = 0x10;
-        const REINITIALIZE = 0x40;
-    }
+/// Audio control flags either commands the host to start, stop
+/// or reinit the audio stream.
+/// Total bits: 32
+#[derive(Debug, Clone, DekuRead, DekuWrite, Eq, PartialEq, Default)]
+pub struct AudioControlFlags {
+    /// Reinit audio stream
+    /// Bit 30 / Mask 0x40000000
+    #[deku(pad_bits_before = "1", bits = "1")]
+    reinitialize: bool,
+
+    /// Start audio stream
+    /// Bit 28 / Mask 0x10000000
+    #[deku(pad_bits_before = "1", bits = "1")]
+    start_stream: bool,
+
+    /// Stop audio stream
+    /// Bit 27 / Mask 0x08000000
+    // Pad to end of 32 bits
+    #[deku(pad_bits_after = "3", pad_bytes_after = "3", bits = "1")]
+    stop_stream: bool,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum AudioDataFlags {}
+pub struct AudioDataFlags {}
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct PCMAudioFormat {
     pub bits: u32,
     pub is_float: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct AudioFormat {
     pub channels: u32,
     pub frequency: u32,
-    pub codec: u32,
+    pub codec: AudioCodec,
+    #[deku(cond = "*codec == AudioCodec::Pcm")]
     pub pcm_format: Option<PCMAudioFormat>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct AudioServerHandshake {
     pub protocol_version: u32,
     pub reference_timestamp: u64,
+    #[deku(update = "self.formats.len()")]
     pub format_count: u32,
-    pub formats: Box<[AudioFormat]>,
+    #[deku(count = "format_count")]
+    pub formats: Vec<AudioFormat>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct AudioClientHandshake {
     pub initial_frame_id: u32,
     pub requested_format: AudioFormat,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct AudioControl {
-    pub flags: u32,
+    pub flags: AudioControlFlags,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct AudioData {
     pub flags: u32,
     pub frame_id: u32,
     pub timestamp: u64,
+    #[deku(update = "self.data.len()")]
     pub data_size: u32,
+    #[deku(count = "data_size")]
     pub data: Vec<u8>,
 }
 
@@ -77,43 +99,67 @@ pub enum AudioPacket {
 
 #[cfg(test)]
 mod tests {
+    use std::default::Default;
+
     use super::*;
 
     #[test]
     fn parse_audio_control_flags() {
-        fn create_flag(val: u32) -> AudioControlFlags {
-            AudioControlFlags::from_bytes(([val], 0))
-                .expect("Failed to create flags")
-                .1
+        fn create_flag(val: [u8; 4]) -> AudioControlFlags {
+            let (rest, flags) =
+                AudioControlFlags::from_bytes((&val, 0)).expect("Failed to create flags");
+
+            // Ensure all bits were processed
+            assert!(rest.0.is_empty());
+            assert_eq!(rest.1, 0);
+
+            flags
         }
-        let start_flag = create_flag(0x40);
-        let stop_flag = create_flag(0x08);
-        let reinit_flag = create_flag(0x40);
-        let start_reinit_flag = create_flag(0x50);
+        let start_flag = create_flag([0x10, 0, 0, 0]);
+        let stop_flag = create_flag([0x08, 0, 0, 0]);
+        let reinit_flag = create_flag([0x40, 0, 0, 0]);
+        let start_reinit_flag = create_flag([0x50, 0, 0, 0]);
 
         assert!(start_flag.start_stream);
-        assert!(!start_flag.stop_flag);
+        assert!(!start_flag.stop_stream);
         assert!(!start_flag.reinitialize);
 
         assert!(!stop_flag.start_stream);
-        assert!(stop_flag.stop_flag);
+        assert!(stop_flag.stop_stream);
         assert!(!stop_flag.reinitialize);
 
+        assert!(!reinit_flag.start_stream);
+        assert!(!reinit_flag.stop_stream);
+        assert!(reinit_flag.reinitialize);
+
         assert!(start_reinit_flag.start_stream);
-        assert!(!start_reinit_flag.stop_flag);
+        assert!(!start_reinit_flag.stop_stream);
         assert!(start_reinit_flag.reinitialize);
     }
 
     #[test]
     fn serialize_audio_control_flags() {
-        let start_flag = AudioControlFlags { start_stream: bool, ..default::Default() };
-        let stop_flag = AudioControlFlags { stop_stream: true, ..default::Default() };
-        let reinit_flag = AudioControlFlags { reinitialize: true, ..default::Default() };
-        let start_reinit_flag = AudioControlFlags { start_stream: true, reinitialize: true, ..default::Default() };
-    
-        assert_eq!(start_flag.to_bytes(), [0x10]);
-        assert_eq!(stop_flag.to_bytes(), [0x08]);
-        assert_eq!(reinit_flag.to_bytes(), [0x40]);
-        assert_eq!(start_reinit_flag.to_bytes(), [0x50]);
+        let start_flag = AudioControlFlags {
+            start_stream: true,
+            ..Default::default()
+        };
+        let stop_flag = AudioControlFlags {
+            stop_stream: true,
+            ..Default::default()
+        };
+        let reinit_flag = AudioControlFlags {
+            reinitialize: true,
+            ..Default::default()
+        };
+        let start_reinit_flag = AudioControlFlags {
+            start_stream: true,
+            reinitialize: true,
+            ..Default::default()
+        };
+
+        assert_eq!(start_flag.to_bytes().unwrap(), vec![0x10, 0, 0, 0]);
+        assert_eq!(stop_flag.to_bytes().unwrap(), vec![0x08, 0, 0, 0]);
+        assert_eq!(reinit_flag.to_bytes().unwrap(), vec![0x40, 0, 0, 0]);
+        assert_eq!(start_reinit_flag.to_bytes().unwrap(), vec![0x50, 0, 0, 0]);
     }
 }
