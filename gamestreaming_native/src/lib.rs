b0fce463cb99695384e4be757882535fec66cc6f
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-10-22 22:12:43 +0200

    refactor: Remove crates bitflags / zerocopy from gamestreaming_native

--- gamestreaming_native/src/lib.rs
+++ gamestreaming_native/src/lib.rs
@@ -1,16 +1,9 @@
 #![allow(dead_code)]
-
-pub extern crate byteorder;
 pub extern crate pnet;
 pub extern crate teredo;
-#[macro_use]
-pub extern crate bitflags;
 
 pub extern crate webrtc;
 
 pub mod crypto;
 pub mod models;
 pub mod packets;
-
-#[cfg(test)]
-mod tests {}
