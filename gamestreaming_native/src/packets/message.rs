03f5826dd7c5d936c1996762816068e477843c41
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-10-22 17:01:55 +0200

    refactor: Port message packets to deku de/serializer

--- gamestreaming_native/src/packets/message.rs
+++ gamestreaming_native/src/packets/message.rs
@@ -1,16 +1,19 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+use deku::prelude::*;
+
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
+#[deku(type = "u32")]
 pub enum MessagePacketType {
     Handshake = 1,
     Data = 2,
     CancelRequest = 3,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct MessageHandshake {
     pub unknown: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct MessageData {
     pub unknown1: u32,
     pub unknown2: u32,
@@ -20,7 +23,7 @@ pub struct MessageData {
     pub unknown6: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, DekuRead, DekuWrite, PartialEq, Eq)]
 pub struct MessageCancelRequest {
     pub unknown: u32,
 }
