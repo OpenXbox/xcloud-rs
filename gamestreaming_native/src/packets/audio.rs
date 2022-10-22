604d51c28ec3b8188153c4b74d59ea0b8bee77e2
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-10-22 16:12:25 +0200

    tests: Parse/Serialize AudioControlFlags

--- gamestreaming_native/src/packets/audio.rs
+++ gamestreaming_native/src/packets/audio.rs
@@ -6,6 +6,7 @@ pub enum AudioPacketType {
     Data = 4,
 }
 
+#[repr(u32)]
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum AudioCodec {
     Opus = 0,
@@ -72,4 +73,47 @@ pub enum AudioPacket {
     ClientHandshake(AudioClientHandshake),
     Control(AudioControl),
     Data(AudioData),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_audio_control_flags() {
+        fn create_flag(val: u32) -> AudioControlFlags {
+            AudioControlFlags::from_bytes(([val], 0))
+                .expect("Failed to create flags")
+                .1
+        }
+        let start_flag = create_flag(0x40);
+        let stop_flag = create_flag(0x08);
+        let reinit_flag = create_flag(0x40);
+        let start_reinit_flag = create_flag(0x50);
+
+        assert!(start_flag.start_stream);
+        assert!(!start_flag.stop_flag);
+        assert!(!start_flag.reinitialize);
+
+        assert!(!stop_flag.start_stream);
+        assert!(stop_flag.stop_flag);
+        assert!(!stop_flag.reinitialize);
+
+        assert!(start_reinit_flag.start_stream);
+        assert!(!start_reinit_flag.stop_flag);
+        assert!(start_reinit_flag.reinitialize);
+    }
+
+    #[test]
+    fn serialize_audio_control_flags() {
+        let start_flag = AudioControlFlags { start_stream: bool, ..default::Default() };
+        let stop_flag = AudioControlFlags { stop_stream: true, ..default::Default() };
+        let reinit_flag = AudioControlFlags { reinitialize: true, ..default::Default() };
+        let start_reinit_flag = AudioControlFlags { start_stream: true, reinitialize: true, ..default::Default() };
+    
+        assert_eq!(start_flag.to_bytes(), [0x10]);
+        assert_eq!(stop_flag.to_bytes(), [0x08]);
+        assert_eq!(reinit_flag.to_bytes(), [0x40]);
+        assert_eq!(start_reinit_flag.to_bytes(), [0x50]);
+    }
 }
