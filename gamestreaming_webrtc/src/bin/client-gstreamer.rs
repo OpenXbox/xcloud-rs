f3ed4ccb61166d973f762d0dc2dc2efd459c188a
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-10-24 23:38:54 +0200

    gst: Import tutorial

--- gamestreaming_webrtc/src/bin/client-gstreamer.rs
+++ gamestreaming_webrtc/src/bin/client-gstreamer.rs
@@ -1,3 +1,106 @@
-fn main() {
-    println!("hello world")
+use gstreamer as gst;
+use gst::prelude::*;
+
+/// macOS has a specific requirement that there must be a run loop running on the main thread in
+/// order to open windows and use OpenGL, and that the global NSApplication instance must be
+/// initialized.
+
+/// On macOS this launches the callback function on a thread.
+/// On other platforms it's just executed immediately.
+#[cfg(not(target_os = "macos"))]
+pub fn run<T, F: FnOnce() -> T + Send + 'static>(main: F) -> T
+where
+    T: Send + 'static,
+{
+    main()
 }
+
+#[cfg(target_os = "macos")]
+pub fn run<T, F: FnOnce() -> T + Send + 'static>(main: F) -> T
+where
+    T: Send + 'static,
+{
+    use cocoa::appkit::NSApplication;
+
+    use std::thread;
+
+    unsafe {
+        let app = cocoa::appkit::NSApp();
+        let t = thread::spawn(|| {
+            let res = main();
+
+            let app = cocoa::appkit::NSApp();
+            app.stop_(cocoa::base::nil);
+
+            // Stopping the event loop requires an actual event
+            let event = cocoa::appkit::NSEvent::otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
+                cocoa::base::nil,
+                cocoa::appkit::NSEventType::NSApplicationDefined,
+                cocoa::foundation::NSPoint { x: 0.0, y: 0.0 },
+                cocoa::appkit::NSEventModifierFlags::empty(),
+                0.0,
+                0,
+                cocoa::base::nil,
+                cocoa::appkit::NSEventSubtype::NSApplicationActivatedEventType,
+                0,
+                0,
+            );
+            app.postEvent_atStart_(event, cocoa::base::YES);
+
+            std::process::exit(0);
+
+            res
+        });
+
+        app.run();
+
+        t.join().unwrap()
+    }
+}
+
+fn tutorial_main() {
+    // Initialize GStreamer
+    gst::init().unwrap();
+
+    // Build the pipeline
+    let uri =
+        "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm";
+    let pipeline = gst::parse_launch(&format!("playbin uri={}", uri)).unwrap();
+
+    // Start playing
+    pipeline
+        .set_state(gst::State::Playing)
+        .expect("Unable to set the pipeline to the `Playing` state");
+
+    // Wait until error or EOS
+    let bus = pipeline.bus().unwrap();
+    for msg in bus.iter_timed(gst::ClockTime::NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.src().map(|s| s.path_string()),
+                    err.error(),
+                    err.debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    // Shutdown pipeline
+    pipeline
+        .set_state(gst::State::Null)
+        .expect("Unable to set the pipeline to the `Null` state");
+}
+
+fn main() {
+    // tutorials_common::run is only required to set up the application environment on macOS
+    // (but not necessary in normal Cocoa applications where this is set up automatically)
+    run(tutorial_main);
+}
+
